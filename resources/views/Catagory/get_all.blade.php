623d272a4180b13f115ef183a5acb27e1732df1b
hanaakalu <hannaakalu28@gmail.com>
2022-05-15 12:34:50 -1000

    our commit

--- resources/views/Catagory/get_all.blade.php
+++ resources/views/Catagory/get_all.blade.php
@@ -9,7 +9,7 @@
  <div class="col-md-8">
  <div class="card">
  <div class="card-header">
- {{ __('YOUR PRODUCT LIST') }}
+ {{ __('our PRODUCT LIST') }}
  </div>
  <div class="card-body">
  <table class="table table-striped table-bordered ">
